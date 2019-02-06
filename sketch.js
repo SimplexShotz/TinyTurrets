d6a7d6dfc2d4a085a6f3c7a2491016e83292dccc
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-06 11:26:13 -0500

    Add Turret and Grid Info

--- sketch.js
+++ sketch.js
@@ -1,6 +1,74 @@
 
 var database, ref;
 
+var colors = [
+  {
+    name: "Red",
+    color: {
+      r: 255,
+      g: 0,
+      b: 0
+    }
+  }
+];
+
+var turrets = [
+  {
+    name: "Turret",
+    health: 20,
+    color: {
+      r: [],
+      g: [],
+      b: []
+    },
+    damage: {
+      initial: 13,
+      operator: "*",
+      amount: 0,
+      cutoff: 0,
+      rounding: round
+    },
+    slowdown: {
+      initial: 0,
+      operator: "*",
+      amount: 0,
+      cutoff: 0,
+      rounding: round
+    },
+    attack: {
+      attacks: true,
+      friendlyfire: false,
+      helpful: false
+    },
+    bullets: {
+      speed: 3,
+      color: {
+        r: 127,
+        g: 127,
+        b: 127
+      }
+    }
+  }
+];
+
+var grid = [];
+for (var y = 0; ++y < 24;) {
+  grid.push([]);
+  for (var x = 0; ++x < 16;) {
+    grid[y].push({
+      turret: -1,
+      colornum: 0,
+      color: {
+        r: 0,
+        g: 0,
+        b: 0
+      },
+      health: 0,
+      cooldown: 0
+    });
+  }
+}
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   // Initialize Firebase
